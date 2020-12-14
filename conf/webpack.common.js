50e5876460bfe7aedf35b0bdc5b19f8930c524c9
Hoang Mirs <hoang.mirs@gmail.com>
2020-12-14 11:35:53 +0700

    Re-add postcss-loader

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -27,7 +27,7 @@ module.exports = {
       // CSS, and Sass
       {
         test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
       },
     ],
   },
