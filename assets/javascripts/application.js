9e9bf7df63c71459678cee46b86e996be97b8a32
Hoang Mirs <hoang.mirs@gmail.com>
2020-12-16 14:20:46 +0700

    Split CSS to improve build time on development

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,5 @@
-require("../stylesheets/application.scss");
+import "../stylesheets/app-base.scss";
+import "../stylesheets/app-components.scss";
+import "../stylesheets/app-utilities.scss";
 
 console.log("Hello from Webpack"); // eslint-disable-line no-console
