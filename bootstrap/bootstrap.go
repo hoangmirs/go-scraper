b428a39ad418e887beca1dc672d83db23691fd4e
Hoang Mirs <hoang.mirs@gmail.com>
2020-12-28 22:18:33 +0700

    Create account and save to DB

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -1,10 +1,12 @@
 package bootstrap
 
 import (
+	"github.com/beego/beego/v2/core/logs"
 	_ "github.com/hoangmirs/go-scraper/models"  // Models
 	_ "github.com/hoangmirs/go-scraper/routers" // Models
 )
 
 func init() {
+	logs.Info("Setup DB")
 	SetUpDB()
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/beego/beego/v2 v2.0.1
 	github.com/lib/pq v1.9.0
 	github.com/smartystreets/goconvey v1.6.4
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
