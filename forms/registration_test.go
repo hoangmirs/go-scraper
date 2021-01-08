4ef5f5b52ba8b678c7a18f275ae836d1a6a12928
Hoang Mirs <hoang.mirs@gmail.com>
2021-01-08 15:41:45 +0700

    Add tests to form

--- forms/registration_test.go
+++ forms/registration_test.go
@@ -3,37 +3,107 @@ package forms_test
 import (
 	"testing"
 
+	"github.com/beego/beego/v2/client/orm"
 	"github.com/beego/beego/v2/core/logs"
+	"github.com/hoangmirs/go-scraper/bootstrap"
+	"github.com/hoangmirs/go-scraper/forms"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestRegistrationForm(t *testing.T) {
+	bootstrap.SetUp()
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Registration Form Suite")
 }
 
 var _ = Describe("RegistrationForm", func() {
-	BeforeEach(func() {
-		logs.Info("before each")
-	})
-
 	AfterEach(func() {
-		logs.Info("after each")
+		o := orm.NewOrm()
+		_, err := o.Raw("TRUNCATE TABLE \"user\"").Exec()
+		if err != nil {
+			// If table can't be truncated, rebuild all tables (CAUTION: Star and Message db are lost!)
+			// This is only for absolute startup
+			err := orm.RunSyncdb("default", true, true)
+			if err != nil {
+				logs.Error(err)
+			}
+		}
 	})
 
-	Describe("Categorizing book length", func() {
-		Context("With more than 300 pages", func() {
-			It("should be a novel", func() {
-				logs.Info("test 1")
-				Expect(1).To(Equal(1))
+	Describe("CreateUser", func() {
+		Context("given valid attributes", func() {
+			It("returns nil", func() {
+				registrationForm := forms.RegistrationForm{
+					Email:                "hoang@nimblehq.co",
+					Password:             "123456",
+					PasswordConfirmation: "123456",
+				}
+				err := registrationForm.CreateUser()
+				Expect(err).To(BeNil())
 			})
 		})
 
-		Context("With fewer than 300 pages", func() {
-			It("should be a short story", func() {
-				logs.Info("test 2")
-				Expect(1).To(Equal(1))
+		Context("given invalid attributes", func() {
+			Context("given a blank email", func() {
+				It("returns the correct error message", func() {
+					registrationForm := forms.RegistrationForm{
+						Email:                "",
+						Password:             "123456",
+						PasswordConfirmation: "123456",
+					}
+					err := registrationForm.CreateUser()
+					Expect(err.Error()).To(Equal("Email Can not be empty"))
+				})
+			})
+
+			Context("given an invalid email", func() {
+				It("returns the correct error message", func() {
+					registrationForm := forms.RegistrationForm{
+						Email:                "hoang-nimblehq.co",
+						Password:             "123456",
+						PasswordConfirmation: "123456",
+					}
+					err := registrationForm.CreateUser()
+					Expect(err.Error()).To(Equal("Email Must be a valid email address"))
+				})
+			})
+
+			Context("given a blank password", func() {
+				It("returns the correct error message", func() {
+					registrationForm := forms.RegistrationForm{
+						Email:                "hoang@nimblehq.co",
+						Password:             "",
+						PasswordConfirmation: "",
+					}
+					err := registrationForm.CreateUser()
+					Expect(err.Error()).To(Equal("Password Can not be empty"))
+				})
+			})
+
+			Context("given an invalid password", func() {
+				It("returns the correct error message", func() {
+					registrationForm := forms.RegistrationForm{
+						Email:                "hoang@nimblehq.co",
+						Password:             "123",
+						PasswordConfirmation: "123",
+					}
+					err := registrationForm.CreateUser()
+					Expect(err.Error()).To(Equal("Password Minimum size is 6"))
+				})
+			})
+
+			Context("given non-matched password and password confirmation", func() {
+				It("returns the correct error message", func() {
+					registrationForm := forms.RegistrationForm{
+						Email:                "hoang@nimblehq.co",
+						Password:             "123456",
+						PasswordConfirmation: "111111",
+					}
+					err := registrationForm.CreateUser()
+					Expect(err.Error()).To(Equal("Password confirmation does not match"))
+				})
 			})
 		})
 	})
