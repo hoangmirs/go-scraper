a3ccb9069531676e83c0c907a20fb971ceb0845e
Hoang Mirs <hoang.mirs@gmail.com>
2021-01-12 18:09:53 +0700

    Add general tests

---
+++ forms/forms_suite_test.go
@@ -0,0 +1,18 @@
+package forms_test
+
+import (
+	"testing"
+
+	"github.com/hoangmirs/go-scraper/bootstrap"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = BeforeSuite(func() {
+	bootstrap.SetUp()
+})
+
+func TestForms(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Forms Suite")
+}
--- helpers/path.go
+++ helpers/path.go
@@ -6,6 +6,7 @@ import (
 	"runtime"
 )
 
+// RootDir returns the current path of the project
 func RootDir() string {
 	_, currentFile, _, _ := runtime.Caller(0)
 	currentFilePath := path.Join(path.Dir(currentFile))
