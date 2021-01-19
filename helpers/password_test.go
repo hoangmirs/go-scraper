a280cc69ce69f9983b7ab611c0745121dae14f8a
Hoang Mirs <hoang.mirs@gmail.com>
2021-01-19 16:47:22 +0700

    Add tests to session form

--- helpers/password_test.go
+++ helpers/password_test.go
@@ -15,4 +15,22 @@ var _ = Describe("Password", func() {
 			})
 		})
 	})
+
+	Describe("ComparePassword", func() {
+		Context("given a correct password", func() {
+			It("returns nil", func() {
+				hashedPassword := "$2a$10$9rnyG9shT0T49i9CBQcnYuICoTwgKvwFCY/EEET63PqjJTat1qHRW"
+				correctPassword := "123456"
+				Expect(helpers.ComparePassword(hashedPassword, correctPassword)).To(BeNil())
+			})
+		})
+
+		Context("given an incorrect password", func() {
+			It("returns error", func() {
+				hashedPassword := "$2a$10$9rnyG9shT0T49i9CBQcnYuICoTwgKvwFCY/EEET63PqjJTat1qHRW"
+				incorrectPassword := "111111"
+				Expect(helpers.ComparePassword(hashedPassword, incorrectPassword)).NotTo(BeNil())
+			})
+		})
+	})
 })
