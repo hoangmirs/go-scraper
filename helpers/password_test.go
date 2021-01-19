c05fcccbe4605ddb28462d041be0c634f8cbbea5
Hoang Mirs <hoang.mirs@gmail.com>
2021-01-19 22:25:56 +0700

    Refactor tests

--- helpers/password_test.go
+++ helpers/password_test.go
@@ -2,6 +2,7 @@ package helpers_test
 
 import (
 	"github.com/hoangmirs/go-scraper/helpers"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
---
+++ tests/db_helper.go
@@ -0,0 +1,25 @@
+package tests
+
+import (
+	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/core/logs"
+)
+
+func ClearUserTable() {
+	o := orm.NewOrm()
+	_, err := o.Raw("TRUNCATE TABLE \"user\"").Exec()
+	if err != nil {
+		// If table can't be truncated, rebuild all tables (CAUTION: Star and Message db are lost!)
+		// This is only for absolute startup
+		ResetDB()
+	}
+}
+
+func ResetDB() {
+	// If table can't be truncated, rebuild all tables (CAUTION: Star and Message db are lost!)
+	// This is only for absolute startup
+	err := orm.RunSyncdb("default", true, true)
+	if err != nil {
+		logs.Error(err)
+	}
+}
