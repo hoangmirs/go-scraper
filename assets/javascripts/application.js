e03840ba10681db8b73deb1bbec235075e6583a5
Hoang Mirs <hoang.mirs@gmail.com>
2021-01-22 16:46:33 +0700

    Fix stylesheet classes

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,3 @@
 import "../stylesheets/app-base.scss";
 import "../stylesheets/app-components.scss";
 import "../stylesheets/app-utilities.scss";
-
-console.log("Hello from Webpack"); // eslint-disable-line no-console
