ad9dd8b621b0ca1a717420b9c9fa151c4163f61d
Hoang Mirs <hoang.mirs@gmail.com>
2021-01-22 23:49:56 +0700

    Change to use Stimulus

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,5 +1,12 @@
+// CSS
 import "../stylesheets/app-base.scss";
 import "../stylesheets/app-components.scss";
 import "../stylesheets/app-utilities.scss";
 
-import "./initializers/";
+// Stimulus
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
--- assets/javascripts/initializers/appHeader.js
+++
@@ -1,9 +0,0 @@
-import AppHeader, {
-  DEFAULT_SELECTOR as HEADER_SELECTOR,
-} from "../components/AppHeader";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const appHeader = document.querySelector(HEADER_SELECTOR);
-
-  appHeader && new AppHeader(appHeader);
-});
