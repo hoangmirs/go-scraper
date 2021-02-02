e48f100f9f3a1cfcd130d4e2ddd8416cfe05975b
Hoang Mirs <hoang.mirs@gmail.com>
2021-02-03 00:35:14 +0700

    Change encryptPassword to hashPassword

--- tests/test_helpers/request_helper.go
+++ tests/test_helpers/request_helper.go
@@ -9,7 +9,7 @@ import (
 	"github.com/onsi/ginkgo"
 )
 
-// MakeRequest make a HTTP request and return response
+// MakeRequest makes a HTTP request and returns response
 func MakeRequest(method string, url string, body io.Reader) *httptest.ResponseRecorder {
 	request, err := http.NewRequest(method, url, body)
 
