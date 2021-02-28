4078bcdf0df0d57243b7ba5e31a365c8ee5a54b0
Hoang Mirs <hoang.mirs@gmail.com>
2021-02-28 23:46:13 +0700

    Remove redundant code

--- models/keyword_result.go
+++ models/keyword_result.go
@@ -1,32 +1,8 @@
 package models
 
 import (
-	"database/sql/driver"
-	"errors"
-
 	"github.com/beego/beego/v2/client/orm"
 )
-
-type KeywordStatus string
-
-const (
-	KeywordType = "keyword"
-
-	Failed     KeywordStatus = "failed"
-	Pending    KeywordStatus = "pending"
-	Processed  KeywordStatus = "processed"
-	Processing KeywordStatus = "processing"
-
-	InvalidKeywordStatusErr = "invalid keyword status"
-)
-
-func (k KeywordStatus) Value() (driver.Value, error) {
-	switch k {
-	case Pending, Processing, Processed, Failed:
-		return string(k), nil
-	}
-	return nil, errors.New(InvalidKeywordStatusErr)
-}
 
 type KeywordResult struct {
 	Base
