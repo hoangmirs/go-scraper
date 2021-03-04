64402daf1686179f6fbb6017878407268ce68181
Hoang Mirs <hoang.mirs@gmail.com>
2021-03-04 23:08:25 +0700

    Set up workers and enqueue the jobs

--- services/keyword/job_enqueuer.go
+++ services/keyword/job_enqueuer.go
@@ -1,23 +1,30 @@
 package keyword
 
-// import (
-// 	"github.com/hoangmirs/go-scraper/bootstrap"
-// 	"github.com/hoangmirs/go-scraper/models"
+import (
+	"github.com/hoangmirs/go-scraper/db"
+	"github.com/hoangmirs/go-scraper/models"
 
-// 	"github.com/beego/beego/v2/core/logs"
-// 	"github.com/gocraft/work"
-// )
+	"github.com/beego/beego/v2/core/logs"
+	"github.com/gocraft/work"
+)
 
-// var enqueuer = work.NewEnqueuer("go-scraper", bootstrap.GetRedisPool())
+var enqueuer *work.Enqueuer
 
-// func EnqueueKeyword(keyword models.Keyword) error {
-// 	job, err := enqueuer.Enqueue("scraper", work.Q{"address": "test@example.com", "subject": "hello world", "customer_id": 4})
+func EnqueueKeyword(keyword models.Keyword) error {
+	setUpEnqueuer()
+	job, err := enqueuer.Enqueue("scrape", work.Q{"keywordID": keyword.Id})
 
-// 	if err != nil {
-// 		return err
-// 	}
+	if err != nil {
+		return err
+	}
 
-// 	logs.Info("Enqueued %v job for keyword %v", job.Name, job.ArgString("keyword"))
+	logs.Info("Enqueued %v job for keyword %v", job.Name, job.ArgString("keyword"))
 
-// 	return nil
-// }
+	return nil
+}
+
+func setUpEnqueuer() {
+	if enqueuer == nil {
+		enqueuer = work.NewEnqueuer("go-scraper", db.GetRedisPool())
+	}
+}
---
+++ workers/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/hoangmirs/go-scraper/bootstrap"
+	"github.com/hoangmirs/go-scraper/db"
+	"github.com/hoangmirs/go-scraper/workers/jobs"
+
+	"github.com/gocraft/work"
+	"github.com/gomodule/redigo/redis"
+)
+
+// Make a redis pool
+var redisPool = &redis.Pool{
+	MaxActive: 5,
+	MaxIdle:   5,
+	Wait:      true,
+	Dial: func() (redis.Conn, error) {
+		return db.GetRedisConnection()
+	},
+}
+
+func init() {
+	bootstrap.SetUp()
+}
+
+func main() {
+	pool := work.NewWorkerPool(jobs.Context{}, 5, "go-scraper", redisPool)
+
+	pool.Middleware((*jobs.Context).ScraperLog)
+
+	pool.JobWithOptions("scrape", work.JobOptions{MaxFails: jobs.MaxFails}, (*jobs.Context).PerformScrape)
+
+	pool.Start()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	<-signalChan
+
+	pool.Stop()
+}
