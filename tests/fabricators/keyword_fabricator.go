a547981ce8ab7f6eeb53afd13f172f6e458118cb
Hoang Mirs <hoang.mirs@gmail.com>
2021-03-08 15:34:45 +0700

    Refactor fabricators

--- tests/fabricators/keyword_fabricator.go
+++ tests/fabricators/keyword_fabricator.go
@@ -2,18 +2,20 @@ package fabricators
 
 import (
 	"github.com/hoangmirs/go-scraper/models"
+
+	"github.com/onsi/ginkgo"
 )
 
-func FabricateKeyword(keyword string, user *models.User) (*models.Keyword, error) {
-	keywordObject := models.Keyword{
+func FabricateKeyword(keyword string, user *models.User) *models.Keyword {
+	keywordObject := &models.Keyword{
 		Keyword: keyword,
 		User:    user,
 	}
 
-	_, err := models.CreateKeyword(&keywordObject)
+	_, err := models.CreateKeyword(keywordObject)
 	if err != nil {
-		return nil, err
+		ginkgo.Fail("Fabricate user error:" + err.Error())
 	}
 
-	return &keywordObject, nil
+	return keywordObject
 }
